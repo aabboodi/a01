e01eac76853db60e7ef772bc60ed871f925703dd
aabboodi <eng.ahmadjadery@gmail.com>
2025-11-29 13:58:22 +0200

    feat: Add `api_constants.dart` to centralize API base URL configuration using `AppConfig`.

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
-// Use your machine's local IP address. Do not use localhost or 127.0.0.1.
-// In Windows, you can find this by running `ipconfig` in the command prompt.
-// In macOS/Linux, you can find this by running `ifconfig` in the terminal.
-const String baseUrl = 'http://192.168.10.158:3000';
+import 'package:frontend/core/config/app_config.dart';
+
+String get baseUrl => AppConfig.apiBaseUrl;
+
